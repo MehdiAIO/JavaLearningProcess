414152f9ffd6693d396a2ccbb6b61c7e1cc56a2f
elmah <elmah@DESKTOP-M25C8SC>
2024-09-28 16:55:52 +0200

    initial commit

---
+++ JavaProject/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("chico ");
+		System.out.print("malo");
+		System.out.print("\n");
+		System.out.println("My name is chico");
+	    System.out.println("\t the end");
+	    System.out.println("\"string\"");
+	}
+}
