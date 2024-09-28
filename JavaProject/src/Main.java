570291ce59dfa5b6f4cc4f9809934368a5e2c603
mehdiaio <elmahdi.aitouzdi@gmail.com>
2024-09-29 00:11:29 +0200

    variables types

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -2,12 +2,50 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("chico ");
-		System.out.print("malo");
-		System.out.print("\n");
-		System.out.println("My name is chico");
-	    System.out.println("\t the end");
-	    System.out.println("\"string\"");
+		// the integer variable can't be longer than 10 
+		int x; // declaration
+		x = 1; // assignment
+		System.out.println(x);
+		
+		int y = 2; // initialization
+		System.out.println(x+y);
+		
+		/* that why we use long variable in case our variable will contain a big number has 
+		 * an 19 character or less but this variable got to have a L letter in the end
+		 */
+		long z = 1218288000000000000L;
+		System.out.println(z);
+		
+		/*  we use float data type to store decimal portion & it got to end with 
+		 *  a common assignment which it is f
+		 */
+		float a = 5.50f;
+		float b = 4.50f;
+		System.out.println(a+b);
+		
+		/*
+		 * or you can you double instead
+		 */
+		double c = 10.05;
+		System.out.println(c);
+		
+		/*
+		 * boolean type only accept true or false
+		 */
+		boolean t = true;
+		System.out.println(t);
+		
+		/*
+		 * char type can't hold more than 1 character
+		 */
+		char ch = '@';
+		System.out.println(ch);
+		
+		/*
+		 * string type able to store more than 1 character
+		 */
+		String st = "mehdi";
+		System.out.println("my name is "+st);
 	}
+	
 }
