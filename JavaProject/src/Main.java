36d7578ec5922f79b6e509f9b8012dc2e98d268c
mehdiaio <elmahdi.aitouzdi@gmail.com>
2024-09-29 18:51:20 +0200

    How to accept user inputs

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -1,17 +1,31 @@
+// import scanner using java utility package
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String x = "water";
-		String y = "orange juice";
-		String empty;
 		
-		empty = x;
-		x = y;
-		y = empty;
+		Scanner scanner = new Scanner(System.in);
+	
+		System.out.println("whats your name");
+		String name = scanner.nextLine();
+		System.out.println("type your age");
+		int age = scanner.nextInt();
 		
-		System.out.println("x: "+x);
-		System.out.println("y: "+y);
+		/* 
+		 * to prevent the issue of escaping the new line we got to make it empty before 
+		 * prompting again
+		 */
+		
+		scanner.nextLine();
+
+		System.out.println("whats your fav food");
+		String food = scanner.nextLine();
+		
+		System.out.println("Good Morning "+name);
+		System.out.println("you have "+age+" years old");
+		System.out.println("your fav food is "+food);
+		
 	}
 	
 }
