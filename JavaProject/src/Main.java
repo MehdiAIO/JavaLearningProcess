c0ae1fc1cab14d9c2fe4e5a4295b4908339adf29
mehdiaio <elmahdi.aitouzdi@gmail.com>
2024-10-06 18:55:40 +0200

    if statements

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -1,16 +1,19 @@
-import java.util.Random;
 public class Main{
 	public static void main(String[] args){
 		
-		Random random = new Random();
+		int age = 11;
 		
-//		int x = random.nextInt(); // generate a number between -x & +x
-//		int x = random.nextInt(6); // generate a number >= 0 & <6
-//		int x = random.nextInt(6)+1; // generate a number >= 0 & <=6
-//		double x = random.nextDouble(); // generate a number > 0 & <1
-		boolean x = random.nextBoolean(); // true or false 
-		
-		System.out.println(x);
-		
-	}
+		if(age>=45) {
+			System.out.println("You're too old !");
+		}
+		else if(age>=18){
+			System.out.println("You're an adult !");
+		}
+		else if(age>=12) {
+			System.out.println("You're a teenager");
+		}
+		else {
+			System.out.println("You're too young");
+		}
+		}
 }
