b86ae2900f606c0ebbc707e2946eb057722879ca
mehdiaio <elmahdi.aitouzdi@gmail.com>
2024-10-12 17:00:15 +0200

    logical operators

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -1,24 +1,18 @@
+import java.util.Scanner;
+
 public class Main{
 	public static void main(String[] args){
 		
-		int day = 0;
+		Scanner scanner = new Scanner(System.in);
 		
-		switch(day) {
-		case 1 : System.out.println("Monday");
-		break;
-		case 2 : System.out.println("Tuesday");
-		break;
-		case 3 : System.out.println("Wednesday");
-		break;
-		case 4 : System.out.println("Thursday");
-		break;
-		case 5 : System.out.println("Friday");
-		break;
-		case 6 : System.out.println("Saturday");
-		break;
-		case 7 : System.out.println("Sunday");
-		break;
-		default : System.out.println("Not found");
+		System.out.println("Press q or Q to quit the game");
+		String response = scanner.next();
+		
+		if(!response.equals("q") && !response.equals("Q")) {
+			System.out.println("Still in the game");
+		}
+		else {
+			System.out.println("You quit the game");
 		}
 		
 	}
