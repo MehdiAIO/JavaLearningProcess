b38f34a858b614cd9ba9ad04bd815855304c90fb
mehdiaio <elmahdi.aitouzdi@gmail.com>
2024-10-13 14:02:37 +0200

    for loop

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -1,18 +1,9 @@
-import java.util.Scanner;
-
 public class Main{
 	public static void main(String[] args){
 		
-		Scanner scanner = new Scanner(System.in);
-		
-		String name = "";
-		
-		while(name.isBlank()) {
-			System.out.println("type your name");
-			name = scanner.nextLine();
+		for(int i=10;i>=0;i-=1) {
+			System.out.println(i);
 		}
-		
-		scanner.close();
 		
 	}
 }
