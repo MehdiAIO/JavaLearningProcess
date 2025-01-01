25bfd2ebd3d282a105dc69ff7270c7e70957d6b8
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-01-01 16:03:10 +0100

    String Methods

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -1,27 +1,20 @@
 public class Main{
 	public static void main(String[] args){
-		// Assigning variables 
-//		String[][] cars = {{"Camaro","AMG","Mclaren"},{"Mustang","Ford","Audi"},{"KIA","Austin Martin","Mazeratti"}};
-		
-		String[][] cars = new String[3][3];
-		
-		cars[0][0] = "Camaro";
-		cars[0][1] = "AMG";
-		cars[0][2] = "Mclaren";
-		cars[1][0] = "Mustang";
-		cars[1][1] = "Ford";
-		cars[1][2] = "Audi";
-		cars[2][0] = "KIA";
-		cars[2][1] = "Austin Martin";
-		cars[2][2] = "Mazeratti";
-		
-		for(int i = 0; i<cars.length;i++) {
-			System.out.println();
-			for(int j = 0; j<cars[i].length;j++) {
-				System.out.print(cars[i][j]+' ');
-			}
-		}
 
+		String name = "Mehdi";
+		String phrase = " Apple Pie ";
+		
+//		boolean result = name.equals("bro");
+//		boolean result = name.equalsIgnoreCase("bro");
+//		int result = name.length();
+//		char result = name.charAt(0);
+//		boolean result = name.isEmpty();
+//		String result = name.toUpperCase();
+//		String result = name.toLowerCase();
+//		String result = phrase.trim();
+		String result = phrase.replace("p","b");
+		
+		System.out.println(result);
 		
 	}
 }
