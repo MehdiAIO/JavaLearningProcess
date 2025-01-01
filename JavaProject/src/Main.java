eef7e68bb52545bd95d515f06dfda14353e57972
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-01-01 14:47:30 +0100

    arrays

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
 		
-		for(int i = 1; i <= 10;i ++) {
-			for(int j = 1; j <= 10; j ++) {
-				System.out.println(i+" x "+j+" = "+i*j);
-			}
+		String[] cars = new String[3];
+		
+		cars[0] = "Camaro";
+		cars[1] = "Corvette";
+		cars[2] = "BMW";
+		
+		for(int i = 0; i < cars.length; i++) {
+			System.out.println(cars[i]);
 		}
 		
 	}
