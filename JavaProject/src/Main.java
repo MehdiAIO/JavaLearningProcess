e55bc4a2b7457424140d692462a37fbc9e8a0942
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-01-03 17:01:45 +0100

    foreach loop

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -3,29 +3,21 @@ import java.util.ArrayList;
 public class Main{
 	public static void main(String[] args){
 
-		ArrayList<ArrayList<String>> groceryList = new ArrayList();
-		
-		ArrayList<String> bakeryList = new ArrayList();
-		bakeryList.add("pasta");
-		bakeryList.add("garlic bread");
-		bakeryList.add("peppers");
-		
-		ArrayList<String> produceList = new ArrayList();
-		produceList.add("tomatoes");
-		produceList.add("zucchini");
-		produceList.add("peppers");
-		
-		ArrayList<String> drinksList = new ArrayList();
-		drinksList.add("soda");
-		drinksList.add("juice");
-		
-		groceryList.add(bakeryList);
-		groceryList.add(0, drinksList);
-		groceryList.add(1,produceList);
-		
-		System.out.println(groceryList);
-		System.out.println(groceryList.get(0));
-		System.out.println(groceryList.get(0).get(1));
+		// for-each = 	traversing technique to iterate through the elements in an array/collection
 
+		//				less steps, more readable
+
+		//				less flexible
+		
+//		String[] cars = {"Mazeratti","Tesla","Ford"};
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Lambo");
+		cars.add("Porshe");
+		cars.add("Audi");
+
+		for(String i : cars) {
+			System.out.println(i);
+		}
+		
 	}
 }
