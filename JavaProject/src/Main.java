102fe2f724d1ae368934608c52ea6b9b14737f3e
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-01-03 16:53:16 +0100

    2D ArrayList

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -2,17 +2,30 @@ import java.util.ArrayList;
 
 public class Main{
 	public static void main(String[] args){
+
+		ArrayList<ArrayList<String>> groceryList = new ArrayList();
 		
-//		ArrayList = resizable array
-		ArrayList<String> players = new ArrayList<String>();
+		ArrayList<String> bakeryList = new ArrayList();
+		bakeryList.add("pasta");
+		bakeryList.add("garlic bread");
+		bakeryList.add("peppers");
 		
-		players.add("Alexander Isak");
-		players.add("Joao Pedro");
-		players.add("Cole Palmer");
+		ArrayList<String> produceList = new ArrayList();
+		produceList.add("tomatoes");
+		produceList.add("zucchini");
+		produceList.add("peppers");
 		
-		for(int i = 0; i < players.size(); i += 1) {
-			System.out.println(players.get(i));
-		}
+		ArrayList<String> drinksList = new ArrayList();
+		drinksList.add("soda");
+		drinksList.add("juice");
 		
+		groceryList.add(bakeryList);
+		groceryList.add(0, drinksList);
+		groceryList.add(1,produceList);
+		
+		System.out.println(groceryList);
+		System.out.println(groceryList.get(0));
+		System.out.println(groceryList.get(0).get(1));
+
 	}
 }
