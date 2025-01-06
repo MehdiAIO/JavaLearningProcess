fa6dccd2a37741f1b3080b5b358bbabedd8638d9
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-01-06 10:36:15 +0100

    printf

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -1,75 +1,26 @@
 public class Main {
 
-
-
 	public static void main(String[] args) {
 
+		boolean myBoolean = true;
+		char myChar = '@';
+		String myString = "Mehdi";
+		int myInt = 7;
+		double myDouble = 10.00;
 		
-
-		// overloaded methods = methods that share the same name but have different parameters
-
-		//						method name + parameters = method signature
-
-		
-
-		double x = add(1.0,2.0,3.0,4.0);
-
-		
-
-		System.out.println(x);
-
-		
-
-	}
-
+//		Conversion characters
+		System.out.printf("%b",myBoolean);
+		System.out.printf("\n%c",myChar);
+		System.out.printf("\n%s",myString);
+		System.out.printf("\n%d",myInt);
+		System.out.printf("\n%f",myDouble);
 	
-
-	static int add(int a, int b) {
-
-		System.out.println("This is overloaded method #1");
-
-		return a + b;
-
-	}
-
-	static int add(int a, int b, int c) {
-
-		System.out.println("This is overloaded method #2");
-
-		return a + b + c;
-
-	}
-
-	static int add(int a, int b, int c, int d) {
-
-		System.out.println("This is overloaded method #3");
-
-		return a + b + c + d;
-
-	}
-
-	static double add(double a, double b) {
-
-		System.out.println("This is overloaded method #4");
-
-		return a + b;
-
-	}
-
-	static double add(double a, double b, double c) {
-
-		System.out.println("This is overloaded method #5");
-
-		return a + b + c;
-
-	}
-
-	static double add(double a, double b, double c, double d) {
-
-		System.out.println("This is overloaded method #6");
-
-		return a + b + c + d;
-
+//		Width
+		System.out.printf("\nBonjour %10s",myString);
+		System.out.printf("\nBonjour %-10s",myString);
+		
+//		Precision
+		System.out.printf("\nvotre montant d'argent : %.2f",2000.109827);
 	}	
 
 }
