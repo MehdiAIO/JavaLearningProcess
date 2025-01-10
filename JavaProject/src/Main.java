61891a8211ce717bd363e38aea3437ab2611e82d
mehdiaio <elmahdi.aitouzdi@gmail.com>
2025-01-10 13:46:57 +0100

    Final keyword

--- JavaProject/src/Main.java
+++ JavaProject/src/Main.java
@@ -2,25 +2,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		boolean myBoolean = true;
-		char myChar = '@';
-		String myString = "Mehdi";
-		int myInt = 7;
-		double myDouble = 10.00;
+		final double PI = 3.1456;
 		
-//		Conversion characters
-		System.out.printf("%b",myBoolean);
-		System.out.printf("\n%c",myChar);
-		System.out.printf("\n%s",myString);
-		System.out.printf("\n%d",myInt);
-		System.out.printf("\n%f",myDouble);
-	
-//		Width
-		System.out.printf("\nBonjour %10s",myString);
-		System.out.printf("\nBonjour %-10s",myString);
+		System.out.println(PI);
 		
-//		Precision
-		System.out.printf("\nvotre montant d'argent : %.2f",2000.109827);
 	}	
 
 }
